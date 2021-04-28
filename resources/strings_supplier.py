fc5af8246142fcee19e2f0145b3ca66cf194c742
Saiful Islam <saiful.abir20@gmail.com>
2021-04-28 12:06:47 +0600

    supplier type model created

---
+++ resources/strings_supplier.py
@@ -0,0 +1,9 @@
+# SupplierType Strings#
+SUPPLIER_TYPE_VERBOSE_NAME = 'Supplier Type'
+SUPPLIER_TYPE_VERBOSE_NAME_PLURAL = 'Supplier Types'
+# SupplierType Strings#
+
+# Supplier Strings#
+FIBER_VERBOSE_NAME = 'Fiber'
+FIBER_VERBOSE_NAME_PLURAL = 'Fibers'
+# Supplier Strings#
--- supplier/models.py
+++ supplier/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from fabric_sample_tracker_api.models import P7Model
+from resources import strings_supplier
 
-# Create your models here.
+
+class SupplierType(P7Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name = strings_supplier.SUPPLIER_TYPE_VERBOSE_NAME
+        verbose_name_plural = strings_supplier.SUPPLIER_TYPE_VERBOSE_NAME_PLURAL
+        db_table = 'supplier_types'
+
+    def __str__(self):
+        return self.name

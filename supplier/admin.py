89f8e72697e8d9728b72d6511f18696e36ab315a
Saiful Islam <saiful.abir20@gmail.com>
2021-05-03 15:10:27 +0600

    supplier added to admin

--- supplier/admin.py
+++ supplier/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from fabric_sample_tracker_api.admin import FabricSampleTrackerAdmin
+from supplier.models import Supplier, Country, SupplierType
 
-# Register your models here.
+
+@admin.register(SupplierType)
+class SupplierTypeAdmin(FabricSampleTrackerAdmin):
+    list_display = ['name', ]
+
+
+@admin.register(Country)
+class CountryAdmin(FabricSampleTrackerAdmin):
+    list_display = ['name', ]
+
+
+@admin.register(Supplier)
+class SupplierAdmin(FabricSampleTrackerAdmin):
+    list_display = ['name', 'supplier_type', 'contact_person_name', 'contact_person_number',
+                    'contact_person_email', 'license_number', 'vat_number'
+                    ]

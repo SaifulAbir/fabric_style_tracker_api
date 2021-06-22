56650e12277e51751485c9753728d051e7b9f807
Saiful Islam <saiful.abir20@gmail.com>
2021-06-22 15:21:02 +0600

    style and fabric details model created

--- resources/strings_style.py
+++ resources/strings_style.py
@@ -2,3 +2,8 @@
 STYLE_VERBOSE_NAME = 'Style'
 STYLE_VERBOSE_NAME_PLURAL = 'Styles'
 # Style Strings#
+
+# Wash Type Strings#
+WASH_TYPE_VERBOSE_NAME = 'Wash Type'
+WASH_TYPE_VERBOSE_NAME_PLURAL = 'Wash Types'
+# Wash Type Strings#
--- style/models.py
+++ style/models.py
@@ -1,13 +1,27 @@
 from django.db import models
-from fabric.models import Fabric
-from fabric_sample_tracker_api.models import FabricSampleTrackerModel
+from django.db.models.signals import pre_save
+from fabric.models import Fabric, FabricDetail
+from fabric_sample_tracker_api.models import FabricSampleTrackerModel, populate_time_info
 from resources import strings_style
 
 
+class WashType(FabricSampleTrackerModel):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name = strings_style.WASH_TYPE_VERBOSE_NAME
+        verbose_name_plural = strings_style.WASH_TYPE_VERBOSE_NAME_PLURAL
+        db_table = 'wash_types'
+
+    def __str__(self):
+        return self.name
+
+
 class Style(FabricSampleTrackerModel):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     fabric = models.ForeignKey(Fabric, on_delete=models.PROTECT, db_column='fabric')
-    wash = models.CharField(max_length=255)
+    fabric_details = models.ForeignKey(FabricDetail, on_delete=models.PROTECT, db_column='fabric_details')
+    wash_type = models.ForeignKey(WashType, on_delete=models.PROTECT, db_column='wash_type')
     designer = models.CharField(max_length=255)
     fob = models.PositiveIntegerField()
     remark = models.TextField(null=True, blank=True)
@@ -19,3 +33,7 @@ class Style(FabricSampleTrackerModel):
 
     def __str__(self):
         return self.name
+
+
+pre_save.connect(populate_time_info, sender=Style)
+pre_save.connect(populate_time_info, sender=WashType)

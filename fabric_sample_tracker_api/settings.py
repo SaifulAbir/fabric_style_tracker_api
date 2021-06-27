3b0aff361911e32b3170231a3d719f5919e2fbd1
Saiful Islam <saiful.abir20@gmail.com>
2021-06-27 11:21:02 +0600

    account app created

--- fabric_sample_tracker_api/settings.py
+++ fabric_sample_tracker_api/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'fabric',
     'supplier',
     'corsheaders',
-    'style'
+    'style',
+    'account'
 ]
 
 MIDDLEWARE = [
@@ -105,6 +106,11 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
